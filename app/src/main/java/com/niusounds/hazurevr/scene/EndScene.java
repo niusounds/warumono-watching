3ffd22da0c91639f1f0e2f6f79b34843ac40c000
Yuya Matsuo <niu@niusounds.com>
2016-05-08 16:14:04 +0900

    全部クリア後に失敗時の音声が鳴る不具合を修正

---
+++ app/src/main/java/com/niusounds/hazurevr/scene/EndScene.java
@@ -0,0 +1,66 @@
+package com.niusounds.hazurevr.scene;
+
+import android.view.View;
+
+import com.eje_c.meganekko.Frame;
+import com.eje_c.meganekko.MeganekkoApp;
+import com.eje_c.meganekko.Scene;
+import com.eje_c.meganekko.SceneObject;
+import com.niusounds.hazurevr.App;
+import com.niusounds.hazurevr.R;
+
+import ovr.JoyButton;
+import ovr.KeyCode;
+
+public class EndScene extends Scene {
+
+    private SceneObject button;
+    private View buttonView;
+
+    @Override
+    protected void initialize(MeganekkoApp app) {
+        super.initialize(app);
+        button = findObjectById(R.id.to_title);
+        buttonView = button.view();
+    }
+
+    @Override
+    public void update(Frame frame) {
+
+        boolean lookingAt = isLookingAt(button);
+        buttonView.setPressed(lookingAt);
+
+        if (lookingAt
+                && JoyButton.contains(frame.getButtonPressed(), JoyButton.BUTTON_TOUCH_SINGLE)) {
+            returnToTitle();
+        }
+
+        super.update(frame);
+    }
+
+    private void returnToTitle() {
+        App app = (App) getApp();
+        app.runOnGlThread(app::toTopScene);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, int repeatCount) {
+
+        if (keyCode == KeyCode.OVR_KEY_BACK) {
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, repeatCount);
+    }
+
+    @Override
+    public boolean onKeyShortPress(int keyCode, int repeatCount) {
+
+        if (keyCode == KeyCode.OVR_KEY_BACK) {
+            returnToTitle();
+            return true;
+        }
+
+        return super.onKeyShortPress(keyCode, repeatCount);
+    }
+}
--- app/src/main/java/com/niusounds/hazurevr/scene/FailureScene.java
+++ app/src/main/java/com/niusounds/hazurevr/scene/FailureScene.java
@@ -1,30 +1,10 @@
 package com.niusounds.hazurevr.scene;
 
-import android.view.View;
-
-import com.eje_c.meganekko.Frame;
-import com.eje_c.meganekko.MeganekkoApp;
-import com.eje_c.meganekko.Scene;
-import com.eje_c.meganekko.SceneObject;
-import com.niusounds.hazurevr.App;
 import com.niusounds.hazurevr.AudioEngine;
-import com.niusounds.hazurevr.R;
 
-import ovr.JoyButton;
-import ovr.KeyCode;
+public class FailureScene extends EndScene {
 
-public class FailureScene extends Scene {
-
-    private SceneObject button;
-    private View buttonView;
     private int soundId;
-
-    @Override
-    protected void initialize(MeganekkoApp app) {
-        super.initialize(app);
-        button = findObjectById(R.id.to_title);
-        buttonView = button.view();
-    }
 
     @Override
     public void onResume() {
@@ -36,45 +16,5 @@ public class FailureScene extends Scene {
     public void onPause() {
         super.onPause();
         AudioEngine.stop(soundId);
-    }
-
-    @Override
-    public void update(Frame frame) {
-
-        boolean lookingAt = isLookingAt(button);
-        buttonView.setPressed(lookingAt);
-
-        if (lookingAt
-                && JoyButton.contains(frame.getButtonPressed(), JoyButton.BUTTON_TOUCH_SINGLE)) {
-            returnToTitle();
-        }
-
-        super.update(frame);
-    }
-
-    private void returnToTitle() {
-        App app = (App) getApp();
-        app.runOnGlThread(app::toTopScene);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, int repeatCount) {
-
-        if (keyCode == KeyCode.OVR_KEY_BACK) {
-            return true;
-        }
-
-        return super.onKeyDown(keyCode, repeatCount);
-    }
-
-    @Override
-    public boolean onKeyShortPress(int keyCode, int repeatCount) {
-
-        if (keyCode == KeyCode.OVR_KEY_BACK) {
-            returnToTitle();
-            return true;
-        }
-
-        return super.onKeyShortPress(keyCode, repeatCount);
     }
 }
